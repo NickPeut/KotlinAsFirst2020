e198cdf8ec9e8abf2e4b355463bdddd027791455
NickPeut <veropeu@mail.ru>
2020-09-13 21:26:37 +0300

    fixed test

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -104,6 +104,5 @@ class Tests {
     fun numberRevert() {
         assertEquals(874, numberRevert(478))
         assertEquals(201, numberRevert(102))
-        assertEquals(201, numberRevert(567))
     }
 }
